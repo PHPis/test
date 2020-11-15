0bc048fe7a8eb47f77221e4f9a8eb536a2689bc1
Tatiana <tanya_yurgina@mail.ru>
2020-11-15 20:15:18 +0700

    refactor

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+
 include __DIR__ . '/vendor/autoload.php';
 include __DIR__ . '/config/config.php';
 include __DIR__ . '/config/routing.php';
